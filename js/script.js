e95863176bb97353fa7f5d080ae001c7500a3b1e
MelaniaPicone <melania_picone@yahoo.it>
2025-07-11 13:55:06 +0200

    [fix] js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,39 @@ return Math.floor(Math.random() * (max - min +1)) +min;
 }
 
 // evento "calcola" al click
+
+calculate.addEventListener('click', (e) => {
+    e.preventDefault();
+
+
+const nameValue = document.getElementById('name').value;
+const kmRun = document.getElementById('km').value;
+const agePass = document.getElementById ('age').value;
+}
+
+
+// calcolo prezzo
+
+const basePrice = 0.21 * kmRun;
+let finalPrice;
+
+if (agePass < '1') {
+  finalPrice = basePrice * 0.8;
+}
+
+else if (agePass === '2') {
+  finalPrice = basePrice *0.6;
+}
+else {
+  finalPrice = baseprice;
+}
+
+const ticketNumber = generateRandomTicketNumber(1000,99999);
+
+results.innerText = `Nome: ${nameValue}
+Numero biglietto: ${ticketNumber}
+Km percorsi: ${kmRun}
+Età passeggero: ${agePass}
+Prezzo finale: € ${finalPrice.toFixed(2)}`;
+
+
